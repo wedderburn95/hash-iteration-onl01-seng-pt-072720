1e06d341432edd726bf2fe71036427a3ca50fd8d
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-09 05:26:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,8 @@
-# birthday_kids = {
-#   "Timmy" => 9,
-#   "Sarah" => 6,
-#   "Amanda" => 27
-# }
+birthday_kids = {
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+}
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
