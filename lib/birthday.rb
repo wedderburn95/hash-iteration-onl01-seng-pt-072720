955c84009274b4c3338af1e8ce0026e7f8517e43
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-09 05:32:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,14 +1,17 @@
-birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
+  
+# birthday_kids = {
+#   "Timmy" => 9,
+#   "Sarah" => 6,
+#   "Amanda" => 27
+# }
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+## bonus
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
